a6842860605d092dd33af89d9405eceb1b1c4392
Jorge Meneses <jorgeluis.menesespaternina@gmail.com>
2023-09-22 07:26:47 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const memu= document.querySelector("#menu");
+const hamburguer= document.querySelector("#hamburguer");
+
+
+
+hamburguer.addEventListener('click',()=>{
+    if(menu.style.display=='none'){
+        menu.style.display='flex';
+    }else{
+        menu.style.display='none';
+    }
+})
